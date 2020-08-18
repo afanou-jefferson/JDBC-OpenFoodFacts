3a52fcb9fe60c87b4b8dcd8326635a09b08c26f4
Jefferon AFANOU <jefferson.afanou@gmail.com>
2020-08-19 00:04:05 +0200

    Changement nom branche

--- tp_jdbc_traitement_fichier/src/main/java/application/ApplicationOpenFoodFacts.java
+++ tp_jdbc_traitement_fichier/src/main/java/application/ApplicationOpenFoodFacts.java
@@ -25,29 +25,7 @@ public class ApplicationOpenFoodFacts {
 	
 
 		File fichier = new File(System.getProperty("user.dir")+"/src/main/resources/open-food-facts.csv");
-		//System.out.println(fichier.canRead());
-		
-		//Datas myDB = new Datas(fichier);
-		
-		//myDB.printListeAllergenes();
-		//myDB.printListeAdditifs(); 
-		//myDB.printListeCategories();
-		//myDB.printListeMarques();
-		
-		//JDBCdaoProduit daoP = new JDBCdaoProduit();
-		//System.out.println(myDB.getListeProduit().get(1423).toString());
-		//daoP.insert(myDB.getListeProduit().get(11423)); // Test Produit
-		
-		
-		//JDBCdaoCategorie daoC = new JDBCdaoCategorie();
-		//daoC.insert(myDB.getListeCategorie().get(0));
-		//System.out.println("win = "  + daoC.getId_Categorie("Additifs alimentaires"));
-		
-		//JDBCdaoAdditif daoA = new JDBCdaoAdditif();
-		//System.out.println(myDB.getListeAdditif().get(0));
-		//daoA.insert(myDB.getListeAdditif().get(0));
-		
-		
+
 		Additif add = new Additif("ex additif");
 		ArrayList<Additif> listAdditif = new ArrayList<Additif>();
 		listAdditif.add(add);
